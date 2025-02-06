c9889a4413bdf4383ee20626eaaff930fc1d4d3d
EwertonLeal <ewertonolivaleal6@gmail.com>
2025-02-05 23:14:23 -0300

    criando models para receber os DTOS do endpoint

---
+++ src/app/models/user-location.model.ts
@@ -0,0 +1,8 @@
+export interface IUserLocation {
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+    timezone: string;
+  }
+  
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,5 +1,16 @@
+import { IUserLocation } from "./user-location.model";
+
 export interface IUser {
     id: string;
+    title: string;
     firstName: string;
     lastName: string;
+    picture: string;
+    gender: string;
+    email: string;
+    dateOfBirth: string;
+    phone: string;
+    location: IUserLocation;
+    registerDate: string;
+    updatedDate: string;
 }
