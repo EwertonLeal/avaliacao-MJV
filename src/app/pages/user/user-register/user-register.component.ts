276aa6ee97d7863daf76c8e82bfe97de1ed99ec2
EwertonLeal <ewertonolivaleal6@gmail.com>
2025-02-05 23:45:27 -0300

    estilização do componente de cadastro

--- src/app/pages/user/user-register/user-register.component.ts
+++ src/app/pages/user/user-register/user-register.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.scss']
 })
-export class UserRegisterComponent {
+export class UserRegisterComponent implements OnInit {
+  userForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildUserForm();
+  }
+
+  onSubmit():void {}
+
+  private buildUserForm(): void {
+    this.userForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName:  ['', [Validators.required, Validators.minLength(2)]],
+      gender:    ['', [Validators.required]],
+      email:     ['', [Validators.required, Validators.email]],
+      dateOfBirth: ['', [Validators.required]], 
+      phone:     [
+        '', 
+        [
+          Validators.required, 
+          Validators.pattern(/^(\(\d{2}\)\s?)?\d{4,5}\-?\d{4}$/)
+        ]
+      ],
+      location: this.fb.group({
+        street:  ['', Validators.required],
+        city:    ['', Validators.required],
+        state:   ['', Validators.required],
+        country: ['', Validators.required],
+        timezone: ['', Validators.required]
+      })
+    });
+  }
 
 }
--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -4,14 +4,20 @@ import { UserListComponent } from './user-list/user-list.component';
 import { UserRoutingModule } from './user.routing.module';
 import { MatIconModule } from '@angular/material/icon';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
 
 
 @NgModule({
   declarations: [
-    UserListComponent
+    UserListComponent,
+    UserRegisterComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     UserRoutingModule,
     NgbPaginationModule,
